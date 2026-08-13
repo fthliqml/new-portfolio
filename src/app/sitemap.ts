bdc6e828cd25b4f1690b1c3d8a47618930259060
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2026-08-13 19:35:51 +0700

    refactor(content): connect project routes to repositories

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,16 +1,21 @@
 import type { MetadataRoute } from "next";
 
+import { getPublicProjects } from "@/data/content/public-content";
 import { siteConfig } from "@/lib/site";
-import { projects } from "@/lib/projects";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const projects = await getPublicProjects();
   const projectPages: MetadataRoute.Sitemap = projects.map((project) => ({
     url: `${siteConfig.url}/projects/${project.id}`,
     lastModified: new Date(),
     changeFrequency: "monthly",
     priority: 0.7,
     images: project.images[0]?.src
-      ? [`${siteConfig.url}${project.images[0].src}`]
+      ? [
+          project.images[0].src.startsWith("http")
+            ? project.images[0].src
+            : `${siteConfig.url}${project.images[0].src}`,
+        ]
       : undefined,
   }));
 
